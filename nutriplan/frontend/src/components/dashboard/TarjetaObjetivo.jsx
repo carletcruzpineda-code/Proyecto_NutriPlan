f7d5dd1030aef7a57df677b40a75f8af1dffa3ee
ccruz1234 <ccruzpinedafwd@gmail.com>
2025-11-16 12:55:51 -0600

    cambio a tarjetas

---
+++ nutriplan/frontend/src/components/dashboard/TarjetaObjetivo.jsx
@@ -0,0 +1,15 @@
+// src/components/dashboard/TarjetaObjetivo.jsx
+export default function TarjetaObjetivo() {
+  return (
+    <div className="card-dash card-objetivo">
+      <div className="obj-header">
+        <span>🎯</span> Indicadores para Mantener Peso
+      </div>
+
+      <div className="obj-body">
+        <h4>2471 cal</h4>
+        <p>Balance</p>
+      </div>
+    </div>
+  );
+}
